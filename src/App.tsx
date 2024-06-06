fc16841d1060e2b7d906d44cd064655145a9ff0a
Jay Cho <jaycho1214@gmail.com>
2024-06-06 16:38:23 -0700

    add padding to bottom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { Divider } from './components';
+import { Cursor, Divider, Navbar, ScrollIndicator } from './components';
 import { About, Home, Projects } from './sections';
 
 export default function App() {
   return (
     <>
       <div className='mt-24' />
+      <Navbar />
       <Home />
       <Divider />
       <About />
       <Divider />
       <Projects />
+      <div className='mt-48' />
+      <ScrollIndicator />
+      <Cursor />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { Cursor, Navbar, ScrollIndicator } from './components';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Navbar />
     <App />
-    <ScrollIndicator />
-    <Cursor />
   </React.StrictMode>,
 );
