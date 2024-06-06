c382c2c10a61616b2ba2659f11e14d3bf2f18e7c
Jay Cho <jaycho1214@gmail.com>
2024-06-06 16:39:51 -0700

    update imports

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -1,10 +1,25 @@
 import { useCallback, useState } from 'react';
 import Modal from 'react-modal';
+
 import DIV2CSUImage from '../assets/images/projects/logo/div2csu.png';
 import MOVCImage from '../assets/images/projects/logo/movc.png';
+import MoliImage from '../assets/images/projects/logo/moli.png';
+import ReflectionImage from '../assets/images/projects/logo/reflection.png';
+import PrayerImage from '../assets/images/projects/logo/prayer.png';
+import IntimeImage from '../assets/images/projects/logo/intime.png';
+import BibleImage from '../assets/images/projects/logo/bible.png';
 
 import { Card, FullScreenModal, Section } from '../components';
-import { MoLi, Reflection, DIV2CSU, MOVC, PCC } from './modals';
+import {
+  MoLi,
+  Reflection,
+  DIV2CSU,
+  MOVC,
+  PCC,
+  Prayer,
+  Bible,
+  Intime,
+} from './modals';
 
 Modal.setAppElement('#root');
 
@@ -14,6 +29,9 @@ const Pages = {
   pcc: <PCC />,
   reflection: <Reflection />,
   div2csu: <DIV2CSU />,
+  prayer: <Prayer />,
+  bible: <Bible />,
+  intime: <Intime />,
 };
 
 export function Projects() {
@@ -31,9 +49,34 @@ export function Projects() {
       <h1 className='text-3xl font-bold md:text-7xl mb-10'>Projects 💻</h1>
       <div className='flex-1 grid grid-cols-2 md:grid-cols-4 gap-4'>
         <Card
-          title='PCC Preview/Bot'
-          src='https://i.imgur.com/987BSZn.jpg'
-          onClick={onClick('pcc')}
+          title='Bible'
+          onClick={onClick('bible')}
+          src={BibleImage}
+        />
+        <Card
+          title='In Time'
+          onClick={onClick('intime')}
+          src={IntimeImage}
+        />
+        <Card
+          title='Prayer'
+          onClick={onClick('prayer')}
+          src={PrayerImage}
+        />
+        <Card
+          onClick={onClick('div2csu')}
+          title='DIV2CSU'
+          src={DIV2CSUImage}
+        />
+        <Card
+          onClick={onClick('reflection')}
+          title='Reflection'
+          src={ReflectionImage}
+        />
+        <Card
+          title='MoLi'
+          onClick={onClick('moli')}
+          src={MoliImage}
         />
         <Card
           title='MO Ventures'
@@ -41,19 +84,9 @@ export function Projects() {
           src={MOVCImage}
         />
         <Card
-          title='MoLi'
-          onClick={onClick('moli')}
-          src='https://crosswand.com/_app/immutable/assets/icon.bf9d18f5.png'
-        />
-        <Card
-          onClick={onClick('reflection')}
-          title='Reflection'
-          src='https://crosswand.com/_app/immutable/assets/icon.d843aa81.png'
-        />
-        <Card
-          onClick={onClick('div2csu')}
-          title='DIV2CSU'
-          src={DIV2CSUImage}
+          title='PCC Preview/Bot'
+          src='https://i.imgur.com/987BSZn.jpg'
+          onClick={onClick('pcc')}
         />
       </div>
       <FullScreenModal
--- src/sections/modals/index.ts
+++ src/sections/modals/index.ts
@@ -3,3 +3,6 @@ export * from './Reflection';
 export * from './DIV2CSU';
 export * from './MOVC';
 export * from './PCC';
+export * from './Prayer';
+export * from './Bible';
+export * from './Intime';
