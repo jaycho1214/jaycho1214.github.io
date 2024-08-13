d51fc0a216d83b85ad8f0b1497f551fe0db3d959
Jay Cho <jaycho1214@gmail.com>
2024-08-13 01:21:38 -0700

    feat: update eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,28 @@
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
+import svelteParser from 'svelte-eslint-parser';
 import prettier from 'eslint-config-prettier';
+import tseslint from 'typescript-eslint';
 import globals from 'globals';
 
 /** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
 	js.configs.recommended,
+	...tseslint.configs.recommended,
 	...svelte.configs['flat/recommended'],
 	prettier,
 	...svelte.configs['flat/prettier'],
+	{
+		files: ['**/*.svelte'],
+		languageOptions: {
+			parser: svelteParser,
+			parserOptions: {
+				parser: {
+					ts: '@typescript-eslint/parser',
+				},
+			},
+		},
+	},
 	{
 		languageOptions: {
 			globals: {
@@ -18,6 +32,6 @@ export default [
 		},
 	},
 	{
-		ignores: ['build/', '.svelte-kit/', 'dist/'],
+		ignores: ['build/', '.svelte-kit/', 'dist/', 'tailwind.config.js'],
 	},
 ];
