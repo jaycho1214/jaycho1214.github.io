813131b2dba5162b8174f29a344cf3400a83d5b7
Jay Cho <jaycho1214@gmail.com>
2024-08-16 00:33:04 -0700

    feat: hide gtag on debug

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,8 +6,28 @@
 	import '../app.css';
 
 	$: if ($navigating) isNavOpened.set(false);
+	const DEBUG = process.env.NODE_ENV === 'DEBUG';
 </script>
 
 <NavBar />
 <slot />
 <Menu />
+
+<svelte:head>
+	{#if !DEBUG}
+		<!-- Google tag (gtag.js) -->
+		<script
+			async
+			src="https://www.googletagmanager.com/gtag/js?id=G-J3472EMQPD"
+		></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag() {
+				dataLayer.push(arguments);
+			}
+			gtag('js', new Date());
+
+			gtag('config', 'G-J3472EMQPD');
+		</script>
+	{/if}
+</svelte:head>
