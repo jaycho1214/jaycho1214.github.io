4b5747bd6e0eefe2f7d07cd2b7505847f8013d95
Jay Cho <jaycho1214@gmail.com>
2024-08-17 01:08:55 -0700

    dev: reload on md file changes

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		{
+			name: 'md-hmr',
+			enforce: 'post',
+			handleHotUpdate: ({ file, server }) => {
+				if (file.endsWith('.md')) {
+					server.ws.send({ type: 'full-reload', path: '*' });
+				}
+			},
+		},
+	],
 });
