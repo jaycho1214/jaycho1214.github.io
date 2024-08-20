1a8e71e46711f8b4952bbdd0c2f71b2afe75ce5d
Jay Cho <jaycho1214@gmail.com>
2024-08-20 15:09:52 -0700

    feat: change dlcard style

--- src/lib/components/DLCard.svelte
+++ src/lib/components/DLCard.svelte
@@ -1,23 +1,28 @@
 <script lang="ts">
-	export let imgSrc: string = '';
+	export let imgSrc: string | null = null;
 	export let category: string = '';
 	export let title: string = '';
 	export let href: string = '';
-	export let textColor: string = '';
 	export let className: string = '';
+	export let backgroundStyle: string | null = null;
 </script>
 
 <a
 	{href}
 	class={`relative text-white rounded inline-block overflow-hidden ${className} w-full h-full`}
-	style={`color: ${textColor}`}
 >
-	<img
-		src={imgSrc}
-		alt="Thumbnail"
-		class="absolute object-cover w-full h-full z-0 pointer-events-none"
-	/>
+	{#if backgroundStyle}
+		<div id="background" style={backgroundStyle} class="absolute inset-0" />
+	{/if}
+	{#if imgSrc}
+		<img
+			src={imgSrc}
+			alt="Thumbnail"
+			class="absolute object-cover w-full h-full z-0 pointer-events-none"
+		/>
+	{/if}
 	<div
+		id="content"
 		class="absolute h-full p-4 flex flex-col justify-between z-10 pointer-events-none"
 	>
 		<p class="font-bold text-sm">{category}</p>
@@ -26,12 +31,19 @@
 </a>
 
 <style>
-	a:hover img {
-		transform: scale(110%);
+	a:hover img,
+	a:hover #background {
+		filter: blur(4px) brightness(40%);
 		transition: all;
 		transition-duration: 400ms;
 	}
-	a img {
+	a:hover {
+		color: white;
+		transition: all;
+		transition-duration: 400ms;
+	}
+	a {
+		color: black;
 		transform: scale(100%);
 		transition: all;
 		transition-duration: 400ms;
